05ce8f802fd3e1d94f36cf81fb273e2c48e5d6fe
Chris <c.hriz.y@hotmail.co.uk>
2019-03-25 15:56:39 +0000

    UI scrpit test

--- Assets/Scripts/PlayerUI.cs
+++ Assets/Scripts/PlayerUI.cs
@@ -1,12 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
 
 public class PlayerIU : MonoBehaviour {
 
+    public Canvas canvas;
+    public GameObject avatar;
+
 	// Use this for initialization
 	void Start () {
-		
+        Vector3 avatarPos = new Vector3(Screen.height / 2, Screen.width / 2, 0);
+        
+        if (PhotonNetwork.IsMasterClient)
+        {
+            canvas.enabled = true;
+        }
+        else
+        {
+            canvas.enabled = false;
+
+            avatar.transform.position = avatarPos;
+        }
 	}
 	
 	// Update is called once per frame
